76df049b08e9e1679254a4d17bd7c7e1f402ec04
Axel <axelborg@kth.se>
2021-12-01 13:58:17 +0100

    one hot encoding

--- evaluate.py
+++ evaluate.py
@@ -10,6 +10,8 @@ def load_cifar10():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
     x_train = (x_train.astype('float32') / 256 ) - 0.5
     x_test =(x_test.astype('float32') / 256 ) - 0.5
+    y_train=tf.keras.utils.to_categorical(y_train, num_classes = 10)
+    y_test=tf.keras.utils.to_categorical(y_test, num_classes = 10)
 
     return x_train, y_train, x_test, y_test
 
--- metrics.py
+++ metrics.py
@@ -6,7 +6,7 @@ from tensorflow.keras.losses import Loss
 
 class NLL(Loss):
     def call(self, y_true, y_pred):
-        loss = tf.keras.losses.sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
+        loss = tf.keras.losses.categorical_crossentropy(y_true, y_pred, from_logits=True)
         nll = tf.reduce_mean(tf.reduce_sum(loss, axis=1))
         return nll
 
