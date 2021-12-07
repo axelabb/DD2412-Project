a5a4b83aef0ff3e57fedd0a1a5601bc987a4bbf3
Axel <axelborg@kth.se>
2021-12-07 11:24:16 +0100

    fix

--- evaluate.py
+++ evaluate.py
@@ -26,7 +26,7 @@ def nll(pred,labels,ensemble_size):
     tiled_labels = np.tile(np.expand_dims(labels,1),[ensemble_size,1]) #10 or 1? 
     nll = -tf.keras.losses.categorical_crossentropy(tiled_labels,pred,from_logits=True)
 
-    return np.mean(np.sum(nll,axis=1)) 
+    return np.mean(-np.log(np.sum(np.exp(nll),axis=1))) 
 
 def member_accuracy(pred,labels,ensemble_size):
     probs = softmax(pred)
@@ -64,8 +64,8 @@ def main(args):
     #print(y_pred.shape)
     print(f"Accuracy: {accuracy(y_pred,y_test)}")
     print(f"NLL: {nll(y_pred,y_test,args.ensemble_size)}")
-    print(f"Member Accuracy: {member_accuracy(y_pred,y_test,args.ensemble_size)}")
-    print(f"Member NLL: {member_nll(y_pred,y_test,args.ensemble_size)} ")
+    #print(f"Member Accuracy: {member_accuracy(y_pred,y_test,args.ensemble_size)}")
+    #print(f"Member NLL: {member_nll(y_pred,y_test,args.ensemble_size)} ")
 
 if __name__=="__main__":
 
